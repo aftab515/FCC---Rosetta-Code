7f44bb87058b196aca20a35c7a47f1b01274632b
Aftab Sadiq <114320334+aftab515@users.noreply.github.com>
2023-01-27 21:49:42 +0500

    Update 100-doors.js
    Added the Q-Statement and Reqs

--- 100-doors.js
+++ 100-doors.js
@@ -1,3 +1,16 @@
+/*
+Statement
+There are 100 doors in a row that are all initially closed. You make 100 passes by the doors. The first time through, visit every door and 'toggle' the door (if the door is closed, open it; if it is open, close it). The second time, only visit every 2nd door (i.e., door #2, #4, #6, ...) and toggle it. The third time, visit every 3rd door (i.e., door #3, #6, #9, ...), etc., until you only visit the 100th door.
+Implement a function to determine the state of the doors after the last pass. Return the final result in an array, with only the door number included in the array if it is open.
+*/ 
+/*
+Requirements
+getFinalOpenedDoors should be a function.
+getFinalOpenedDoors should return an array.
+getFinalOpenedDoors should produce the correct result.
+*/ 
+
+
 function getFinalOpenedDoors(numDoors) {
   let doors = [];
   let i = 1, ii = i*i;
