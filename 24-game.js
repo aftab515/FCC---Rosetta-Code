3d021a3fbe41b582d3ef7e46a48895481ff6725d
Aftab Sadiq <114320334+aftab515@users.noreply.github.com>
2023-01-28 08:49:07 +0500

    Update 24-game.js
    Added Q-statement and Reqs

--- 24-game.js
+++ 24-game.js
@@ -1,3 +1,24 @@
+/*
+Statement
+The 24 Game tests a person's mental arithmetic.
+The aim of the game is to arrange four numbers in a way that when evaluated, the result is 24
+Implement a function that takes a string of four digits as its argument, with each digit from 1 to 9 (inclusive) with repetitions allowed, and returns an arithmetic expression that evaluates to the number 24. If no such solution exists, return "no solution exists".
+
+Rules:
+Only the following operators/functions are allowed: multiplication, division, addition, subtraction.
+Division should use floating point or rational arithmetic, etc, to preserve remainders.
+Forming multiple digit numbers from the supplied digits is disallowed. (So an answer of 12+12 when given 1, 2, 2, and 1 is wrong).
+The order of the digits when given does not have to be preserved.
+*/ 
+
+/*
+Requirements
+solve24 should be a function.
+solve24("4878") should return (7-8/8)*4, 4*(7-8/8), or a similar valid string
+solve24("1234") should return 1*2*3*4 or a similar valid string
+solve24("6789") should return (6*8)/(9-7), (8*6)/(9-7), or a similar valid string
+solve24("1127") should return (1+7)*(1+2) or a similar valid string
+*/ 
 function solve24(numStr) {
   const digitsArr = numStr.split('');
   const answers = [];
